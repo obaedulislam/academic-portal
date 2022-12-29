1b146bf5cd4d150c8f63c3cd8e3e87063ff03c13
obayedul islam <obaed.cse@gmail.com>
2022-12-29 23:37:19 +0600

    Banner added successfully

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
-import Banner from "./Home/banner";
+import Banner from "./Home/Banner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div>
       <div className="mt-12">
-        <Banner></Banner>
+        <Banner />
       </div>
     </div>
   );
